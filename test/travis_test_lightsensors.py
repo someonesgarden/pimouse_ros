9fd389e13e6ce20ed543a73040c2eeb0bca00ce7
someonesgarden <d@someonesgarden.org>
2021-01-19 04:42:53 +0000

    fixed

---
+++ test/travis_test_lightsensors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#encoding: utf8
+import unittest, rostest
+import rosnode, rospy
+import time
+from pimouse_ros.msg import LightSensorValues
+
+class LightsensorTest(unittest.TestCase):
+    pass
+
