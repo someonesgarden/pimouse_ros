433bc838a85fff3619ea4a06638e2e99de35ac4a
someonesgarden <d@someonesgarden.org>
2021-01-20 07:04:54 +0000

    buzzer test fixed

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -39,7 +39,7 @@ class BuzzerTest(unittest.TestCase):
         self.client.wait_for_result()
 
         self.assertTrue(self.client.get_result(), "invalid result")
-        self.assertEqual(goal.freqs, self.device_values, "invalid feedback:" + ",".join([str(e) for e in self.device_values]))
+        self.assertEqual(goal.freqs, self.device_values, "invalid feedback:,".join([str(e) for e in self.device_values]))
         
         ###preemptiion###
         self.device_values = []
